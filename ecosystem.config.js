3d40fbfb05ceb241e48ed81168f3a6c40393d327
README.md <314502289+hy781457-botupclub@users.noreply.github.com>
2026-08-20 01:10:45 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-  apps: [
-    {
-      name: 'wingo-panel',
-      script: 'index.js',
-      instances: 1,
-      exec_mode: 'fork',
-      restart_delay: 2000,
-      max_restarts: 10,
-      env: {
-        NODE_ENV: 'production',
-        PORT: 4000
-      }
-    }
-  ]
-};
+
